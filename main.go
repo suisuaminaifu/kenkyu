a7e32a9c3c6f00ad95b485e3d44ab2aee9e44bba
suisuaminaifu <suisuaminaifu@gmail.com>
2024-11-30 20:28:20 +0800

    add PDF text extraction functionality and include test PDF file

--- main.go
+++ main.go
@@ -1,7 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"os/exec"
+)
+
+func extractTextFromPdf(pdfPath string) (string, error) {
+	args := []string{
+		"-layout", // Maintain (as best as possible) the original physical layout of the text.
+		pdfPath,
+		"-", // Send the output to stdout.
+	}
+
+	cmd := exec.CommandContext(context.TODO(), "pdftotext", args...)
+
+	var buf bytes.Buffer
+	cmd.Stdout = &buf
+
+	if err := cmd.Run(); err != nil {
+		return "", err
+	}
+
+	return buf.String(), nil
+}
 
 func main() {
 	fmt.Println("Hello, World from Kenkyu!")
+
+	result, err := extractTextFromPdf("test.pdf")
+	if err != nil {
+		fmt.Println("Error extracting text from PDF:", err)
+		return
+	}
+
+	fmt.Println(result)
 }
